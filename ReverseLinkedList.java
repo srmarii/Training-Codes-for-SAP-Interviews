2a739e54ff6e1070712c440e85242982a4bd1413
mariana ramos <marisr2005@gmail.com>
2026-07-01 17:46:05 -0300

    added two new ones

---
+++ ReverseLinkedList.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ReverseLinkedList {
+    public int[] reverseTheList(int list[]){
+        int tam = list.length, newList[] = new int[tam], k=0;
+
+        for(int i = tam -1; i >= 0; i--){
+            newList[i] = list[k];
+            k++;
+        }
+
+        return newList;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList r = new ReverseLinkedList();
+        int list[] = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(r.reverseTheList(list)));
+    }
+}
