ec5c7d75e9a3d847e968331520ef4829420436b6
mariana ramos <marisr2005@gmail.com>
2026-07-20 17:21:30 -0300

    resolved "anagram.java" in the right way

---
+++ hashmap/Anagram.java
@@ -0,0 +1,60 @@
+package hashmap;
+/*
+An Anagram is a word or phrase formed by rearranging the letters of a 
+different word or phrase, typically using all the original letters exactly once.
+ 
+Write a method that two strings and compare them to checks if the first string 
+is a anagram of second string.
+*/
+
+import java.util.HashMap;
+
+public class Anagram {
+ 
+  public static void main(String args[]) {
+ 
+      //pass
+      String result1 = isAnagram("sap", "pas") ? "Pass": "Fail";
+      System.out.println(result1);
+      //fail
+      String result2 = isAnagram("anagram", "nagaram") ? "Pass": "Fail";
+      System.out.println(result2);
+      //fail
+      String result3 = isAnagram("car", "rat") ? "Pass" : "Fail";
+      System.out.println(result3);
+      String result4 = isAnagram("", "") ? "Pass" : "Fail";
+      System.out.println(result4);
+      String result5 = isAnagram("", "house") ? "Pass": "Fail";
+      System.out.println(result5);
+      String result6 = isAnagram("house", "") ? "Pass": "Fail";
+      System.out.println(result6);
+  }
+
+
+  public static boolean isAnagram(String s1, String s2) {
+    HashMap<Character, Integer> hm1 = new HashMap<>();
+    HashMap<Character, Integer> hm2 = new HashMap<>();
+
+    int tam1 = s1.length(), tam2 = s2.length(), i;
+
+    if(tam1 == tam2 && tam1 > 1){
+        for(i = 0; i<tam1; i++){
+            Character charAtual = s1.charAt(i);
+            hm1.put(charAtual, hm1.getOrDefault(charAtual, 0) +1);
+        }
+
+        for(i = 0; i<tam1; i++){
+            Character charAtual = s2.charAt(i);
+            hm2.put(charAtual, hm2.getOrDefault(charAtual, 0) +1);
+        }
+
+        return hm1.equals(hm2);
+    } else{
+        return false;
+    }
+
+
+    }
+  }
+    
+
